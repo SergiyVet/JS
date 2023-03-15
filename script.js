2698d69d479a9dbd6c1d3eebb09c41f83f25581b
SergiyVet <98781599+SergiyVet@users.noreply.github.com>
2023-03-15 14:04:17 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+/*
+let year =  +prompt("Year of birth");
+alert("Your age "+ (2023 - year));
+*/
+
+/*
+let radius = +prompt(" Radius = ");
+area = Math.PI* radius**2;
+alert("Area = "+area);
+*/
+
+/*
+let l = +prompt(" Distance in km = ");
+let t = +prompt(" Time in hours = ");
+let v = l/t;
+alert("Speed "+v+" km/h");
+*/
+
+/*
+let dolar = +prompt(" Input dolar ");
+let euro = 0.93 * dolar;
+alert(" Euro "+ euro);
+*/
+
+/*
+let flashGb = +prompt("Flash drive in GB = ");
+let flashMb = 1000*flashGb;
+alert(Math.floor(flashMb/820));
+*/
