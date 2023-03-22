8fc2f6e739c05e8e44066867eaf15055676ec9d3
SergiyVet <98781599+SergiyVet@users.noreply.github.com>
2023-03-22 10:32:34 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -27,3 +27,23 @@ let flashGb = +prompt("Flash drive in GB = ");
 let flashMb = 1000*flashGb;
 alert(Math.floor(flashMb/820));
 */
+
+/*
+let kilomtr = +prompt("Input kilometer = ");
+let miles = 0.621371*kilomtr;
+alert("Miles = "+ miles);
+*/
+ /*
+let hours = +prompt("How much now hours?");
+let minutes = +prompt("How much now minutes?");
+hours = 24-hours;
+minutes=60-minutes;
+alert("There are "+hours+" hours and "+minutes+" minutes left until the end of the day");
+*/
+/*
+ let number = +prompt(" Input 3-digit number");
+ let rezult1 = number%10;
+ let rezult2 = Math.floor(number/10)%10;
+ let rezult3 = Math.floor(number/100)
+ alert("Rezult = "+ rezult1+rezult2+rezult3);
+*/
