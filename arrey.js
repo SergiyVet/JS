41f0a3d8071ec9a16a6150789b5ca2b749207c9d
SergiyVet <sergiyvitruk11@gmail.com>
2023-03-30 23:29:14 +0300

    create 4,5,6

--- arrey.js
+++ arrey.js
@@ -33,5 +33,38 @@ arr.sort((a, b) => a - b);
 arr.reverse();
 console.log(arr);
 */
+//4
+/*
+var arr = new Array(20); 
 
+for (let i = 0; i < arr.length; i++) {
+  arr[i] = Math.floor(Math.random() * 100) + 1; 
+}
+let index = arr.length / 2;
+arr.fill(0,index);
+console.log(arr);
+*/
+//5
+/*
+var arr = new Array(20); 
+
+for (let i = 0; i < arr.length; i++) {
+  arr[i] = Math.floor(Math.random() * 100) + 1; 
+}
+let ar = arr.filter((a) => a % 7 ==0 );
+console.log(ar);
+console.log(arr);
+*/
+
+//6
+/*
+var arr = new Array(20); 
+
+for (let i = 0; i < arr.length; i++) {
+  arr[i] = Math.floor(Math.random() * 100) + 1; 
+}
+console.log(arr);
+arr.splice(0,3);
+console.log(arr);
+*/
 
