975fa535f48679bc76a0f905cafc3a3eb2c453a6
SergiyVet <sergiyvitruk11@gmail.com>
2023-03-30 23:15:11 +0300

    create 2 and 3

--- arrey.js
+++ arrey.js
@@ -1,3 +1,5 @@
+//Task 1
+//1
 /*
 const arr = new Array(20); 
 
@@ -7,3 +9,29 @@ for (let i = 0; i < arr.length; i++) {
 
 console.log(arr);
 */
+
+//2
+/*
+const arr = new Array(20); 
+
+for (let i = 0; i < arr.length; i++) {
+  arr[i] = Math.floor(Math.random() * 100) + 1; 
+  console.log(`[${i+1}] - ${arr[i]}`);
+}
+
+console.log(arr);
+*/
+
+//3
+/*
+var arr = new Array(20); 
+
+for (let i = 0; i < arr.length; i++) {
+  arr[i] = Math.floor(Math.random() * 100) + 1; 
+}
+arr.sort((a, b) => a - b);
+arr.reverse();
+console.log(arr);
+*/
+
+
