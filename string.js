338271da87c0e0a5b2eaf0dedf0f1537a9e97d04
SergiyVet <sergiyvitruk11@gmail.com>
2023-03-31 22:54:08 +0300

    create 3 and 4

--- string.js
+++ string.js
@@ -12,3 +12,19 @@ let string = prompt("Input text");
 string[0].toUpperCase();
 console.log(string[0].toUpperCase()+string.slice(1, string.length));
 */
+//3
+/*
+let string = prompt("Input text");
+let words = string.split(' ');
+console.log(words.length);
+*/
+//4
+/*
+let string = prompt("Input text");
+let words = string.split(' ');
+let arr = [];
+for (const item of words) {
+    arr.push(item[0].toUpperCase());
+}
+console.log(arr.join(''));
+*/
