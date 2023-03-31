21a27f19d515c2f27cb5dcbcdfa87572a6138c0a
SergiyVet <sergiyvitruk11@gmail.com>
2023-03-31 22:18:50 +0300

    creat 1 and 2

---
+++ string.js
@@ -0,0 +1,14 @@
+//Task 1
+//1
+/*
+let string = prompt("Input text");
+let words = string.split(" ");
+let arr = words.join('');
+console.log(string.length - arr.length);
+*/
+//2
+/*
+let string = prompt("Input text");
+string[0].toUpperCase();
+console.log(string[0].toUpperCase()+string.slice(1, string.length));
+*/
